7cfba86e28bd4fbad92824513511d3a9eaf6f5e0
Fredrik Fahlstad <fredrik@fahlstad.se>
2023-01-12 21:57:42 +0100

    from home

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,23 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProjectsUsers;
 use App\Models\Task;
+use App\Models\User;
+
 use Illuminate\Support\Facades\Auth;
 use function response;
 
 
 class TaskController extends Controller
 {
-    function getAll(\Illuminate\Http\Request $request)
-    {
-        $tasks = Task::whereBelongsTo(Auth::user())
 
+    function getTask($id)
+    {
+        $task = Task::where("tasks.id", $id)
             ->leftJoin("projects", "projects.id", "=", "tasks.project_id")
             ->leftJoin("projects_users", "projects_users.project_id", "=", "projects.id")
             ->leftJoin("users", "tasks.assigned_user_id", "=", "users.id")
+            ->select(
+                "tasks.*",
+                "users.name as assigned_user_name",
+                "users.id as assigned_user_id",
+                "users.image_url",
+                "projects.name as project",
+                "projects.color as project_color")
+            ->first();
 
+        return $task;
+    }
+
+    function getAll(\Illuminate\Http\Request $request)
+    {
+        $tasks = Task::whereBelongsTo(Auth::user())
+            ->leftJoin("projects", "projects.id", "=", "tasks.project_id")
+            ->leftJoin("projects_users", "projects_users.project_id", "=", "projects.id")
+            ->leftJoin("users", "tasks.assigned_user_id", "=", "users.id")
             ->orWhere("projects_users.user_id", Auth::id())
-
             ->select(
                 "tasks.*",
                 "users.name as assigned_user_name",
@@ -27,9 +46,6 @@ class TaskController extends Controller
                 "projects.name as project",
                 "projects.color as project_color")
             ->distinct()->get();
-
-
-
 
 
         return response()->json(
@@ -43,12 +59,7 @@ class TaskController extends Controller
             $request->all()
         );
 
-        $task = Task::whereBelongsTo(Auth::user())
-            ->where("tasks.id", $new_task->id)
-            ->leftJoin("projects", "projects.id", "=", "tasks.project_id")
-            ->select("tasks.*", "projects.name as project", "projects.color as project_color")->first();
-
-        return response()->json($task, 201);
+        return response()->json($this->getTask($new_task->id), 201);
     }
 
     function update($id, \Illuminate\Http\Request $request)
@@ -57,31 +68,8 @@ class TaskController extends Controller
         $task = Task::findOrFail($id);
         $task->update($d);
 
-//        $task = Task::whereBelongsTo(Auth::user())
-//            ->where("tasks.id", $task->id)
-//            ->leftJoin("projects", "projects.id", "=", "tasks.project_id")
-//            ->leftJoin("users", "tasks.user_id", "=", "users.id")
-//
-//            ->select("users.image_url", "tasks.*","users.name as assigned_user_name", "projects.name as project", "projects.color as project_color")->first();
-//
 
-        $task = Task::where("tasks.id", $id)
-            ->leftJoin("projects", "projects.id", "=", "tasks.project_id")
-            ->leftJoin("projects_users", "projects_users.project_id", "=", "projects.id")
-            ->leftJoin("users", "tasks.assigned_user_id", "=", "users.id")
-
-            ->select(
-                "tasks.*",
-                "users.name as assigned_user_name",
-                "users.id as assigned_user_id",
-                "users.image_url",
-                "projects.name as project",
-                "projects.color as project_color")
-            ->first();
-
-
-
-        return response()->json($task, 200);
+        return response()->json($this->getTask($task->id), 201);
     }
 
     function delete($id)
